d87c4b6e216235f604b796601d77b16c0fce409e
Diego de Sousa Brandão <74800323+DSB88@users.noreply.github.com>
2021-10-26 20:28:16 -0300

    Update Dividir.java

--- src/br/com/fatec/engsoftware/designpatterns/semana09/strategy/exemplopessoal/Dividir.java
+++ src/br/com/fatec/engsoftware/designpatterns/semana09/strategy/exemplopessoal/Dividir.java
@@ -4,9 +4,10 @@ public class Dividir implements ICalcular {
     @Override
     public int calcular(int a, int b) {
         try {
-            return a/b;
-        }catch (Exception e){
-            System.err.print("Erro, divisão não possível! ");
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.err.print("Erro, na divisão, calculo não possível! " +
+                    "\nErro relatado: "+e );
             return 0;
         }
     }
